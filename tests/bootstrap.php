2716e54f6d40391dd717d84cbf2fe90834955454
Cauchy Cai <cauchy.cai@gmail.com>
2015-07-06 10:51:10 +0800

    Using Composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
-require __DIR__ . '/Psr4Autoloader.php';
 
-$autoloader_1435908479 = new Psr4Autoloader();
-$autoloader_1435908479->addNamespace('WechatCrypto\\', __DIR__ . '/../src/');
-$autoloader_1435908479->addNamespace('WechatCrypto\\Tests\\', __DIR__ . '/');
-$autoloader_1435908479->register();
+$loader = require __DIR__.'/../vendor/autoload.php';
